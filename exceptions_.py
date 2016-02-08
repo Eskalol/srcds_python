49b05588ef2db0aac0a6d5a9330d5f9462c3c364
Eskil Opdahl Nordland <eskil.nordland@gmail.com>
2016-02-08 21:56:31 +0100

    fixed file names

---
+++ exceptions_.py
@@ -0,0 +1,13 @@
+"""
+	
+"""
+
+class InvalidPacketType(Exception):
+	"""This Exception is thrown when a user pass a invalid packet_type to send function"""
+	def __str__(self):
+		"""returns str of the Exception
+		
+		Returns:
+		    str: a usefull message
+		"""
+		return repr('Invalid packet type, must be 3 or 2')
